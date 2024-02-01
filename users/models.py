7acdce0eaf6f98b26db5154413211ca1d51402d7
Pedrolivares1980 <128540589+Pedrolivares1980@users.noreply.github.com>
2024-02-01 17:03:35 +0000

    stilyng

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.core.files.storage import default_storage as storage
+from io import BytesIO
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,13 +10,26 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
-    # This method is used to save the profile picture and resize it if necessary
+
+    # This method is overridden to save the profile picture and resize it if necessary.
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        # Open the image using the Django's storage system
+        if self.image:
+            img_read = storage.open(self.image.name, 'rb')
+            img = Image.open(img_read)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+
+                # Save the modified image to a BytesIO object
+                in_mem_file = BytesIO()
+                img.save(in_mem_file, format='JPEG')
+                img_read.close()
+
+                # Save the modified image back to storage
+                img_write = storage.open(self.image.name, 'wb+')
+                img_write.write(in_mem_file.getvalue())
+                img_write.close()
