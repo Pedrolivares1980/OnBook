1e63ccc5bcff4e071e43e9f798f9409d00c55258
Pedrolivares1980 <128540589+Pedrolivares1980@users.noreply.github.com>
2024-02-01 10:36:40 +0000

    Update settings and remove populate_books.py

--- OnBook/settings.py
+++ OnBook/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'TRUE'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('django-onbook.onrender.com', '')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_HOSTNAME', '')]
 
 # Application definition
 
