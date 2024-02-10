dd82a9322ab1dfecf5484ec61c2c234a1c77ff79
Pedrolivares1980 <128540589+Pedrolivares1980@users.noreply.github.com>
2024-02-10 17:00:07 +0000

    debug

--- OnBook/settings.py
+++ OnBook/settings.py
@@ -13,8 +13,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG') == 'TRUE'
 
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_HOSTNAME', '')]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_HOSTNAME', '')]
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
