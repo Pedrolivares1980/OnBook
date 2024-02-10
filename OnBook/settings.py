8deda4a0fbc6b4e9a8b72c07c84e40dafe2b3e5e
Pedrolivares1980 <128540589+Pedrolivares1980@users.noreply.github.com>
2024-02-10 16:40:48 +0000

    allowed hosts

--- OnBook/settings.py
+++ OnBook/settings.py
@@ -13,7 +13,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG') == 'TRUE'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_HOSTNAME', '')]
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_HOSTNAME', ''), 'https://django-onbook.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
